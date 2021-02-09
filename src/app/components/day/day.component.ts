d8608e175b27a2a66f0a25a8119f0c97c666edc1
mmartinez <marcmm1994@gmail.com>
2021-02-09 12:18:17 +0100

    Implement current day style

--- src/app/components/day/day.component.ts
+++ src/app/components/day/day.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Day } from 'src/app/models/day.model';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-day',
@@ -9,10 +10,19 @@ import { Day } from 'src/app/models/day.model';
 export class DayComponent implements OnInit {
 
   @Input('day') day: Day;
+  public isToday: boolean = false;
   
   constructor() { }
 
   ngOnInit(): void {
+    this.checkDateIsToday();
+  }
+  
+  public checkDateIsToday() {
+    const auxDate = moment(new Date());
+    if (this.day.date.format('DD/MM/YYYY') === auxDate.format('DD/MM/YYYY')) {
+      this.isToday = true;
+    }
   }
 
 }
