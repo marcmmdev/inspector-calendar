cd04cb1c677f72009746387d2700346bba83f8e5
mmartinez <marcmm1994@gmail.com>
2021-02-10 14:00:30 +0100

    Implement calendar testing

--- src/app/components/calendar/calendar.component.spec.ts
+++ src/app/components/calendar/calendar.component.spec.ts
@@ -1,4 +1,7 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Observable } from 'rxjs';
+import { WeatherService } from 'src/app/services/weather.service';
 
 import { CalendarComponent } from './calendar.component';
 
@@ -6,11 +9,18 @@ describe('CalendarComponent', () => {
   let component: CalendarComponent;
   let fixture: ComponentFixture<CalendarComponent>;
 
+  let weatherService: WeatherService;
+  let httpMock: HttpClient;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CalendarComponent ]
-    })
-    .compileComponents();
+      declarations: [ CalendarComponent ],
+      imports: [HttpClientModule],
+      providers: [WeatherService]
+    }).compileComponents();
+
+    weatherService = TestBed.inject(WeatherService);
+    httpMock = TestBed.inject(HttpClient);
   });
 
   beforeEach(() => {
@@ -22,4 +32,23 @@ describe('CalendarComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should retrieve inspection days', () => {
+    expect(component.inspectionDays).toBeTruthy();
+  });
+
+  it('should fill selected month calendar', () => {
+    expect(component.calendar).toBeTruthy();
+  });
+
+  it ('should call get forecast from WeatherService', () => {
+    const weatherService = fixture.debugElement.injector.get(WeatherService);
+    const weatherSpy = spyOn(weatherService, 'getFiveDaysForecast').and.callThrough();
+    fixture.detectChanges();
+    component.getForecast();
+
+    expect(weatherSpy).toHaveBeenCalled(); // error
+    expect(weatherSpy.calls.any()).toBeTruthy(); // error
+  });
+
 });
